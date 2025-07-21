69091dbfce52925df5cdc10750f0ec7b59d7f83d
harshitttiwari <tiwariharshit990@gmail.com>
2025-07-21 22:22:06 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,126 @@
+import base64
+import requests
+import constants
+from PIL import Image
+import streamlit as st
+
+API_URL = "https://arpy8-plant-detection-api.hf.space/predict"
+
+
+# def predict_class(image_path):
+#     original_image = Image.open(image_path)
+
+#     preprocessed_image = original_image.resize((256, 256))
+#     preprocessed_image = np.array(preprocessed_image) / 255.0
+
+#     preds = model.predict(np.expand_dims(preprocessed_image, axis = 0))
+#     labels = ['Healthy', 'Powdery', 'Rust']
+
+#     preds_class = np.argmax(preds)
+#     preds_label = labels[preds_class]
+
+#     return preds_label, round(preds[0][preds_class], 2)
+
+
+def predict_class_from_api(image_path):
+    files = {'file': image_path}
+    headers = {'accept': 'application/json'}
+
+    try:
+        response = requests.post(API_URL, headers=headers, files=files)
+        response.raise_for_status()
+        result = response.json()
+        
+        return result
+        
+    except requests.exceptions.HTTPError as err:
+        print(err)
+        return "Error", 0.0
+
+################## STREAMLIT APP ##################
+
+@st.cache_data()
+def get_base64_of_bin_file(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+def set_png_as_page_bg(png_file):
+    bin_str = get_base64_of_bin_file(png_file)
+    page_bg_img = '''
+    <style>
+    body {
+    background-image: url("data:image/png;base64,%s") !important;
+    background-size: cover !important;
+    }
+    </style>
+    ''' % bin_str
+    
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+    return
+
+set_png_as_page_bg('assets/bg.webp')
+
+st.write("<h1>CROP CARE - AI Based Plant Disease Detection using Image Classification</h1>", unsafe_allow_html=True)
+
+
+with st.sidebar:
+    st.image("assets/logo.png")
+    
+    uploaded_file = st.file_uploader("Upload an image", type=['png', 'jpg', 'jpeg'])
+    
+    st.info("""
+        # Team Members
+        Om Kumar Singh  
+        Roshmik Agrawal  
+        Aadish Chaturvedi  
+        Harshit Tiwari  
+        Abhyanand Sharma  
+
+        # About
+        This is a web app to predict the disease in a plant using Convolutional Neural Network (CNN).
+        
+        # Contact
+        For inquiries, you can mail us [here](mailto:kumar.23bai10076@vitbhopal.ac.).
+        """)
+
+home_page = st.empty()
+home_page.write(constants.FRONT_PAGE, unsafe_allow_html=True)
+
+display_image = st.empty()
+classifying_text = st.empty()
+button = st.empty()    
+
+if uploaded_file is not None:
+    home_page.empty()
+    display_image.image(uploaded_file, width=450)
+    
+    predict_button = button.button("Predict", use_container_width=True)
+
+    if predict_button and uploaded_file is not None: 
+        classifying_text.empty()
+        button.empty()
+        with st.spinner("Predicting..."):
+            label = predict_class_from_api(uploaded_file)
+
+
+        st.toast("Prediction Complete!")
+        st.info(f"""
+##### Predicted Class:
+{label}
+
+##### Description: 
+{constants.DISEASE_DESCRIPTION[label]}
+
+##### Fertilizer Recommended: 
+{constants.FERTILIZER_RECOMMENDATIONS[label]}
+
+##### Details:
+{constants.FERTILIZER_DETAILS[label]}
+        
+##### REFERENCE:
+{constants.REFERENCE[label]}     
+        """)
+
+
+classifying_text.empty()
---
+++ constants.py
@@ -0,0 +1,58 @@
+MODEl_PATH = 'weights/plant_disease_classifier.h5'
+FRONT_PAGE = """
+<body style="font-family: Arial, sans-serif; color: #333; font-size: 16px;">
+    <p> CROP CARE - AI Based Plant Disease Detection using Image Classification Streamlit web application. This tool is designed to assist you in identifying the health status of your plants with the help of a Convolutional Neural Network (CNN). Whether you are a home gardener or a farmer, early detection of plant diseases can make a significant difference in preserving the well-being of your plants and optimizing crop yields.</p>
+    <h2>How It Works</h2>
+    <ol>
+        <li><strong>Upload an Image:</strong> To get started, simply upload an image of your plant. The image should be clear and centered on the plant.</li>
+        <li><strong>Prediction:</strong> Once you've uploaded an image, click the "Predict" button. Our pre-trained CNN model will analyze the image to determine whether the plant is "Healthy," affected by "Powdery Mildew," or has signs of "Rust."</li>
+        <li><strong>Confidence Score:</strong> The application will provide you with a confidence score, indicating how sure the model is about its prediction.</li>
+    </ol>
+    <h2>Why Use This App?</h2>
+    <ul>
+        <li><strong>Early Disease Detection:</strong> Detecting plant diseases early can help prevent the spread of diseases and reduce crop losses.</li>
+        <li><strong>Easy to Use:</strong> You don't need to be an expert in plant pathology. This user-friendly tool simplifies the process of disease detection.</li>
+        <li><strong>Quick Results:</strong> In just a few seconds, you'll receive a classification result with a confidence score.</li>
+    </ul>
+    <h2>About</h2>
+    <p>This web application is built using Streamlit and powered by a deep learning model. It offers a user-friendly interface for plant disease detection, making it accessible to anyone interested in plant health.</p>
+    <h2>Get Started</h2>
+    <p>To begin using this application, follow these steps:</p>
+    <ol>
+        <li>Click the "Upload an Image" button in the sidebar.</li>
+        <li>Select an image of your plant from your device.</li>
+        <li>Click the "Predict" button to see the classification result.</li>
+    </ol>
+    <p>Whether you are a professional farmer or a hobbyist gardener, this tool can help you keep your plants healthy and thriving. Try it out and make informed decisions about plant care!</p>
+    <h2>Acknowledgments</h2>
+    <p>We acknowledge the efforts of the open-source community in developing and sharing the model used in this application. This tool would not be possible without their contributions.</p>
+    <p>If you have any feedback or suggestions for improving this application, we welcome your input. Together, we can help plants thrive and promote sustainable agriculture.</p>
+    <p>Thank you for using the "CROP CARE - AI Based Plant Disease Detection using Image Classification" web app!</p>
+</body>
+"""
+
+
+DISEASE_DESCRIPTION = {
+    "Healthy": "Your plant appears to be healthy! Keep up the good work with your plant care routine to maintain its health and vitality. Regular watering, proper sunlight exposure, and monitoring for pests and diseases can help your plant thrive.",
+    "Powdery": "Powdery mildew is a fungal disease that affects a wide range of plants, including fruits, vegetables, and ornamental plants. It appears as a white or gray powdery growth on the leaves, stems, and flowers of infected plants. Powdery mildew can weaken the plant, reduce fruit yield, and affect the overall health of the plant. It is important to treat powdery mildew early to prevent its spread and protect the plant from further damage.",
+    "Rust": "Rust is a common plant disease caused by various fungi. It appears as orange, yellow, or reddish-brown pustules on the leaves, stems, or fruit of infected plants. Rust can weaken the plant, reduce photosynthesis, and affect the overall health of the plant. It is important to treat rust early to prevent its spread and protect the plant from further damage.",
+}
+
+FERTILIZER_RECOMMENDATIONS = {
+    "Healthy": "No fertilizer required",
+    "Powdery": "Sulfur-based fungicide",
+    "Rust": "Copper-based fungicide"
+       
+    }
+FERTILIZER_DETAILS ={
+    "Healthy":"Your plant is healthy and does not require additional fertilizers. Continue regular care practices like watering, proper sunlight, and occasional checks for pests.",
+    "Powdery":"Sulfur-based fungicides are effective against powdery mildew. Apply as directed to control fungal growth and protect the plant from further damage.",
+    "Rust":"Copper-based fungicides are recommended to treat rust. Use sparingly to prevent resistance and ensure proper plant recovery."
+
+}    
+REFERENCE ={
+    "Healthy":"https://www.wikihow.com/Take-Care-of-Plants",
+    "Powdery":"https://eos.com/blog/powdery-mildew",
+    "Rust":"https://whyfarmit.com/copper-fungicide/"
+}    
+
